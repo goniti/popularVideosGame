c64b4935ca0720e53b67134ce14dfd2cee91bfee
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-07-06 15:29:10 +0200

    set environment variable API KEY

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 import { Normalize } from 'styled-normalize'
 import '../../styles/font.css'
 
+
 const GlobalStyle = createGlobalStyle`
   body {
 		font-family: 'Roboto', sans-serif;
@@ -9,7 +10,9 @@ const GlobalStyle = createGlobalStyle`
 	}
 `
 
+
 function App() {
+    console.log(process.env.REACT_APP_API_KEY)
   return (
     <div className="App">
       <Normalize />
