cbb37a9426f8701b56dcfd77235ca2a7a61e22e9
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-07-06 16:00:26 +0200

    add pagination and set page count

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {createGlobalStyle} from 'styled-components'
 import {Normalize} from 'styled-normalize'
 import '../../styles/font.css'
+import { Pagination } from '@material-ui/lab';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,9 +15,10 @@ function App() {
     const [error, setError] = useState(false)
     const [loading, setLoading] = useState(true)
     const [dataGame, setDataGame] = useState([])
+    const pageCount = Math.ceil(dataGame.count/20)
 
     useEffect(() => {
-        fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&dates=2020-01-01,2020-12-31`)
+        fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&dates=2020-01-01,2020-12-31&platforms=18,1,7`)
             .then(response => response.json())
             .then(json => {
                 setDataGame(json)
@@ -32,11 +34,11 @@ function App() {
 
     if (loading && dataGame.length === 0) return <span>Loading...</span>
     if (error) return <span>Error while loading data...</span>
-    console.log("dataGame", dataGame)
   return (
     <div className="App">
       <Normalize />
       <GlobalStyle />
+        <Pagination count={pageCount} />
     </div>
   )
 }
