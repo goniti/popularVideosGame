cf6d176e5bdf469ff294d1cdba625afbe728f7b1
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-07-06 15:41:11 +0200

    send request api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
-import { createGlobalStyle } from 'styled-components'
-import { Normalize } from 'styled-normalize'
+import {useEffect, useState} from "react";
+import {createGlobalStyle} from 'styled-components'
+import {Normalize} from 'styled-normalize'
 import '../../styles/font.css'
-
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -10,9 +10,29 @@ const GlobalStyle = createGlobalStyle`
 	}
 `
 
-
 function App() {
-    console.log(process.env.REACT_APP_API_KEY)
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const [dataGame, setDataGame] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&dates=2020-01-01,2020-12-31`)
+            .then(response => response.json())
+            .then(json => {
+                setDataGame(json)
+                setError(false)
+            })
+            .catch(() => {
+                setError(true)
+            })
+            .finally(() => {
+                setLoading(true)
+            })
+    }, [])
+
+    if (loading && dataGame.length === 0) return <span>Loading...</span>
+    if (error) return <span>Error while loading data...</span>
+    console.log("dataGame", dataGame)
   return (
     <div className="App">
       <Normalize />
